7e038e66bb894b961bd2c3b30261b6e2ef5d0ce3
Aquiles-bb <brunoaquiles@ufpr.br>
2023-02-12 01:24:16 -0300

    Maior parte concluida
    Necessita mais testes

--- natal.c
+++ natal.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct gift{
+typedef struct objetos{
     int valor;
     int peso;
-}gift_t;
+}objetos_t;
 
 typedef struct presentes{
-    int num_gifts;
+    int num_objetos;
     int p_max;
-    gift_t *gifts;
+    objetos_t *objetos;
 }presentes_t;
 
 typedef struct saco{
@@ -21,56 +21,109 @@ typedef struct saco{
 /*Cria uma struct presentes_t e retorna um ponteiro para ela.*/
 presentes_t *cria_presentes_t(){
     presentes_t *presentes = malloc(sizeof(presentes_t));
-    presentes->num_gifts = 0;
+    presentes->num_objetos = 0;
     presentes->p_max = 0;
-    presentes->gifts = NULL;
+    presentes->objetos = NULL;
 
     return presentes;
 }
 
 /*Destroi struct @presentes.(grinch odeia natal)*/
 presentes_t *grinch(presentes_t *presentes){
-    free(presentes->gifts);
+    free(presentes->objetos);
     free(presentes);
 
     return NULL;
 }
 
-/*Le os gifts e coloca na struct @presentes.*/
+/*Le os objetos e coloca na struct @presentes.*/
 void le_presentes(presentes_t *presentes){
-    scanf("%d", &presentes->num_gifts);
-    scanf("%d", &presentes->p_max);
-    presentes->gifts = malloc(sizeof(gift_t)*presentes->num_gifts);
-    for (int i = 0; i < presentes->num_gifts; i++) {
-        scanf("%d %d", &presentes->gifts[i].valor, &presentes->gifts[i].peso);
+    scanf("%d %d", &presentes->num_objetos, &presentes->p_max);
+    presentes->objetos = malloc(sizeof(objetos_t)*presentes->num_objetos);
+    for (int i = 0; i < presentes->num_objetos; i++) {
+        scanf("%d %d", &presentes->objetos[i].valor, &presentes->objetos[i].peso);
     }
 }
 
-/*Debug*/
+/*Imprime informacoes sobre os presentes.*/
 void imprime_presentes(presentes_t *prese){
-    printf ("%d\n", prese->num_gifts);
-    printf ("%d\n", prese->p_max);
-    for (int i = 0; i < prese->num_gifts; i++) {
-        printf ("%d %d\n", prese->gifts[i].valor, prese->gifts[i].peso);
+    system("clear");
+    printf ("\nNumero de objetos: %d\n", prese->num_objetos);
+    printf ("Peso maximo: %d\n", prese->p_max);
+    printf ("\nNº objeto -> valor  peso\n");
+    for (int i = 0; i < prese->num_objetos; i++) {
+        printf ("   %dº obj -> %5d %5d\n", i+1, prese->objetos[i].valor, prese->objetos[i].peso);
     }
+    printf ("\n");
+}
+
+/*Devolve o peso do saco.*/
+int peso_do_saco(saco_t *saco, presentes_t *prese){
+    int peso = 0;
+    for (int i = 0; i < saco->tam; i++) {
+        peso += prese->objetos[saco->melhor_subcjt_gifts[i]].peso;
+    }
+
+    return peso;
+}
+
+/*Devolve o valor do saco.*/
+int valor_do_saco(saco_t *saco, presentes_t *prese){
+    int valor = 0;
+    for (int i = 0; i < saco->tam; i++) {
+        valor += prese->objetos[saco->melhor_subcjt_gifts[i]].valor;
+    }
+
+    return valor;
+}
+
+/*Passa informacoes do novo melhor saco.*/
+void atualiza_melhor_saco(saco_t *saco, saco_t *novo_saco){
+    saco->valor = novo_saco->valor;
+    saco->tam = novo_saco->tam;
+    free(saco->melhor_subcjt_gifts);
+    saco->melhor_subcjt_gifts = novo_saco->melhor_subcjt_gifts;
+}
+
+/*Compara dois sacos e atualiza o melhor.*/
+void compara_sacos(saco_t *saco, saco_t *aux_saco, presentes_t *prese){
+    int peso = peso_do_saco(aux_saco, prese);
+    aux_saco->valor = valor_do_saco(aux_saco, prese);
+
+    if (peso > prese->p_max){
+        free(aux_saco->melhor_subcjt_gifts);
+        return;
+    }
+    if (aux_saco->valor > saco->valor){
+        atualiza_melhor_saco(saco, aux_saco);
+        return;
+    }
+    else if (aux_saco->valor == saco->valor && peso < peso_do_saco(saco, prese)) {
+        atualiza_melhor_saco(saco, aux_saco);
+        return;
+    }
+
+    free(aux_saco->melhor_subcjt_gifts);
 }
 
 /*Cria o subconjunto de presentes da vez.*/
-void analisa_subconjunto(int *vet, presentes_t *presentes, saco_t *melhor_saco){
-    saco_t aux_saco;
-    aux_saco.melhor_subcjt_gifts = malloc(sizeof(presentes->num_gifts));
-    aux_saco.tam = 0;
-    for (int i = 0; i < presentes->num_gifts; i++) {
-        if (vet[i] == 1)
-            aux_saco.melhor_subcjt_gifts[aux_saco.tam++] = i+1;
+void analisa_subconjunto(int *vet, presentes_t *presentes, saco_t *cria_melhor_saco){
+    saco_t *aux_saco = malloc(sizeof(saco_t));
+    aux_saco->melhor_subcjt_gifts = malloc(sizeof(int)*presentes->num_objetos);
+    aux_saco->tam = 0;
+    for (int i = 0; i < presentes->num_objetos; i++) {
+        if (vet[i] == 1){
+            aux_saco->melhor_subcjt_gifts[aux_saco->tam] = i;
+            aux_saco->tam++;
+        }
     }
-
-    free(aux_saco.melhor_subcjt_gifts);
+    compara_sacos(cria_melhor_saco, aux_saco, presentes);
+    free(aux_saco);
 }
 
-/*Cria todos os subconjuntos de presentes.*/
+/*Cria todos os subconjuntos de presentes com backtracking.*/
 void acha_melhor_subconjunto_gifts(int *vet, presentes_t *prese, int indice, saco_t *saco){
-    if (indice == prese->num_gifts){
+    if (indice == prese->num_objetos){
         analisa_subconjunto(vet, prese, saco);
         return;
     }
@@ -81,22 +134,56 @@ void acha_melhor_subconjunto_gifts(int *vet, presentes_t *prese, int indice, sac
 }
 
 /*Retorna uma struct saco_t com o melhor subconjunto dos presentes.*/
-saco_t *melhor_saco(presentes_t *presentes){
-    int vet[presentes->num_gifts];
+saco_t *cria_melhor_saco(presentes_t *presentes){
+    if (presentes->objetos == NULL)
+        return NULL;
+    int vet[presentes->num_objetos];
     saco_t *saco = malloc(sizeof(saco_t));
-    saco->melhor_subcjt_gifts = malloc(sizeof(presentes->num_gifts));
+    saco->valor = -1;
+    saco->tam = 0;
+    saco->melhor_subcjt_gifts = NULL;
 
     acha_melhor_subconjunto_gifts(vet, presentes, 0, saco);
-    
+
     return saco;
 }
 
-int main(){
-    int tam;
-    presentes_t *presentes = cria_presentes_t();
-    /* le_presentes(presentes); */
+/*Destroi struct saco_t.*/
+saco_t *destroi_saco(saco_t *saco){
+    free(saco->melhor_subcjt_gifts);
+    free(saco);
 
-    grinch(presentes);
+    return NULL;
+}
+
+/*Imprime os presentes e o valor do saco.*/
+void imprime_saco (saco_t *saco){
+    for (int i = 0; i < saco->tam; i++) {
+        printf ("%d ", saco->melhor_subcjt_gifts[i]+1);
+    }
+    printf ("\n%d\n", saco->valor);
+}
+
+/*Imprime os presentes e o valor do saco de forma mais detalhada.*/
+void imprime_saco_detalhado (saco_t *saco){
+    printf ("Objetos escolhidos: ");
+    for (int i = 0; i < saco->tam; i++) {
+        printf ("%dº ", saco->melhor_subcjt_gifts[i]+1);
+    }
+    printf ("\nValor total: %d\n", saco->valor);
+}
+
+int main(){
+    presentes_t *presentes = cria_presentes_t();
+    le_presentes(presentes);
+    imprime_presentes(presentes);
+
+    saco_t *saco = cria_melhor_saco(presentes);
+    /* imprime_saco(saco); */
+    imprime_saco_detalhado(saco);
+
+    presentes = grinch(presentes);
+    saco = destroi_saco(saco);
 
     return 0;
 }
